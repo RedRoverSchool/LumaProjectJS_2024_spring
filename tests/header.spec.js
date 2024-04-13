b370295e33a481d81dd7176db7236f2551f1077b
Andrei <davydov8580@gmail.com>
2024-04-13 22:13:06 +0400

    TC 01.1.1_05 User can navigate

--- tests/header.spec.js
+++ tests/header.spec.js
@@ -3,24 +3,7 @@ import { test, expect } from "@playwright/test";
 test.describe('header', () => {
   const BASE_URL = "https://magento.softwaretestingboard.com";
 
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/');
-  })
-
-  test('verify Home page title', async ({ page }) => {
-
-    await expect(page).toHaveTitle('Home Page');
-  })
-
-  test('verify user can navigate to home page clicking on logo from "What\'s New" page', async ({ page }) => {
-    await page.getByRole('listitem').filter({ hasText: "What's New" }).click();
-    await expect(page).toHaveURL(BASE_URL + '/what-is-new.html');
-
-    await page.getByLabel('store logo').click();
-    await expect(page).toHaveURL(BASE_URL);
-  })
-
-  test('navigate to home page clicking on logo from "What\'s New" page', async ({ page }) => {
+    test('verify user can navigate to home page clicking on logo from "What\'s New" page', async ({ page }) => {
     await page.getByRole('listitem').filter({ hasText: "What's New" }).click();
     await expect(page).toHaveURL(BASE_URL + '/what-is-new.html');
 
