ad2804b3773c319a6961395a5e7d50af3b719f07
AlexUA2022 <avsaponov@gmail.com>
2024-04-14 09:47:35 +0300

    fixed test

--- tests/menuTraining.spec.js
+++ tests/menuTraining.spec.js
@@ -8,7 +8,7 @@ test.describe('menuTraining', () => {
         await page.goto('/');
     })
     
-    test.only('Verify that the "Training" link redirects to the training\'s products page', async({page}) => {
+    test('Verify that the "Training" link redirects to the training\'s products page', async({page}) => {
         
         await expect(page).toHaveURL(BASE_URL);
 		  await expect(page.locator('#ui-id-7')).toBeVisible();
