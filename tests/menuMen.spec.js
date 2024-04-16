5a7d34d8bd9736c10b36d7981bd907a9983ce593
Светлана Чупрасова <chuprasovasvetlana@gmail.com>
2024-04-16 12:03:45 +0300

    rename test

--- tests/menuMen.spec.js
+++ tests/menuMen.spec.js
@@ -3,15 +3,15 @@ import { test, expect } from '@playwright/test';
 test.describe('menuMen', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
-  });
-  test('Verify navigation from the menu to page Men', async ({ page }) => {
+  })
+   test('Verify navigation from the menu to page Men', async ({ page }) => {
     await page.getByRole('menuitem', { name: 'Men' }).last().click();
 
     await expect(page).toHaveURL('https://magento.softwaretestingboard.com/men.html');
     await expect(page.getByRole('heading', { name: 'Men' })).toBeVisible();
-  });
+   })
 
-  test("Verify the display of comparison products on the men's page", async ({ page }) => {
+   test("Verify the display of comparison products on the men's page", async ({ page }) => {
     await page.getByRole('link', { name: 'Sign In' }).click();
     await page.getByLabel('Email').fill('svetik.buratino@gmail.com');
     await page.getByLabel('Password').fill('User123!');
@@ -23,5 +23,5 @@ test.describe('menuMen', () => {
     const countItemsBlockCompare = page.locator('#compare-items li');
 
     await expect(countItemsBlockCompare).toHaveCount(3);
-  });
+   });
 });
