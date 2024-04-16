1151b6ecaad6e1df6a132d8dfe61e3352d6e98c7
Alena <51464730+alenavolakhava@users.noreply.github.com>
2024-04-16 14:42:51 -0400

    refactor: remove unnecessary variables and lines

--- tests/gearBags.spec.js
+++ tests/gearBags.spec.js
@@ -1,18 +1,13 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("gearBags", () => {
-  const baseURL = "https://magento.softwaretestingboard.com/";
-
   test.beforeEach(async ({ page }) => {
     await page.goto("/");
   });
   test("Verify material sidebar menu option exists", async ({ page }) => {
-    await page.goto(baseURL);
-
     page.locator("#ui-id-6").hover();
     await page.locator("#ui-id-25").click();
-    let title = await page.title();
-    expect(title).toEqual("Bags - Gear");
+    await expect(page).toHaveTitle("Bags - Gear");
     expect(page.getByText("Material")).toBeVisible();
   });
 });
