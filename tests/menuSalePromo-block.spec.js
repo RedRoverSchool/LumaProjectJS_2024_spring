3b5ae91dd5b45a86e4bdcb2e303ae3ad524cff90
Valeriia <gudzenkoleruska@gmail.com>
2024-04-16 20:29:31 -0500

    refactored test: moved 5 tests in 1 test using array

--- tests/menuSalePromo-block.spec.js
+++ tests/menuSalePromo-block.spec.js
@@ -1,12 +1,13 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("menuSalePromo-block", () => {
-
-  const hoodiesAndSweatshirts_url = 'https://magento.softwaretestingboard.com/men/tops-men/hoodies-and-sweatshirts-men.html';
-  const jackets_url = 'https://magento.softwaretestingboard.com/men/tops-men/jackets-men.html';
-  const tees_url = 'https://magento.softwaretestingboard.com/men/tops-men/tees-men.html';
-  const pants_url = 'https://magento.softwaretestingboard.com/men/bottoms-men/pants-men.html';
-  const shorts_url = 'https://magento.softwaretestingboard.com/men/bottoms-men/shorts-men.html';
+  const links = [
+    { name: 'Hoodies and Sweatshirts', url: 'https://magento.softwaretestingboard.com/men/tops-men/hoodies-and-sweatshirts-men.html', title: 'Hoodies & Sweatshirts' },
+    { name: 'Jackets', url: 'https://magento.softwaretestingboard.com/men/tops-men/jackets-men.html', title: 'Jackets' },
+    { name: 'Tees', url: 'https://magento.softwaretestingboard.com/men/tops-men/tees-men.html', title: 'Tees' },
+    { name: 'Pants', url: 'https://magento.softwaretestingboard.com/men/bottoms-men/pants-men.html', title: 'Pants' },
+    { name: 'Shorts', url: 'https://magento.softwaretestingboard.com/men/bottoms-men/shorts-men.html', title: 'Shorts' }
+  ];
 
   test.beforeEach(async ({ page }) => {
     await page.goto("/" + "sale.html");
@@ -19,42 +20,11 @@ test.describe("menuSalePromo-block", () => {
     expect(promoBlocks).toHaveCount(6);
   });
 
-  test('Check that Hoodies and Sweatshirts link opens the corresponding page', async ({page}) =>{
-    await page.getByRole('link',{name:'Hoodies and Sweatshirts'}).last().click();
-
-    await expect(page).toHaveURL(hoodiesAndSweatshirts_url);
-    await expect(page.locator('#page-title-heading > span')).toHaveText('Hoodies & Sweatshirts');
-});
-
-  test('Check that Jackets link opens the corresponding page', async ({page}) =>{
-    await page.getByRole('link',{name:'Jackets'}).last().click();
-
-    await expect(page).toHaveURL(jackets_url);
-    await expect(page.locator('#page-title-heading > span')).toHaveText('Jackets');
-});
-
-  test('Check that Tees link opens the corresponding page', async ({page}) =>{
-    await page.getByRole('link',{name:'Tees'}).last().click();
-
-    await expect(page).toHaveURL(tees_url);
-    await expect(page.locator('#page-title-heading > span')).toHaveText('Tees');
-
-});
-
-  test('Check that Pants link opens the corresponding page', async ({page}) =>{
-    await page.getByRole('link',{name:'Pants'}).last().click();
-
-    await expect(page).toHaveURL(pants_url);
-    await expect(page.locator('#page-title-heading > span')).toHaveText('Pants');
-  });
-
-  test('Check that Shorts link opens the corresponding page', async ({page}) =>{
-    await page.getByRole('link',{name:'Shorts'}).last().click();
-  
-    await expect(page).toHaveURL(shorts_url);
-    await expect(page.locator('#page-title-heading > span')).toHaveText('Shorts');
-    
-  
-});
-
+  for (const link of links) {
+  test(`Check that ${link.name} link opens the corresponding page`, async ({ page }) => {
+      await page.getByRole('link', { name: link.name }).last().click();
+      await expect(page).toHaveURL(link.url);
+      await expect(page.locator('#page-title-heading > span')).toHaveText(link.title);
+    });
+  }
 })
