928182c0393c2886943e4028726e122a4ab23450
trukhachev495 <sergey.trainings@gmail.com>
2024-04-17 20:02:26 +0400

    TC 06.1.4_02 Verifies that the list is selected and contains 10 items by default.

--- tests/menTopsDisplayMode.spec.js
+++ tests/menTopsDisplayMode.spec.js
@@ -12,4 +12,14 @@ test("Checking that the grid is selected and has 12 positions by default", async
     await expect(page.locator('li[class = "item product product-item"]')).toHaveCount(12)
     await expect(page.locator('#maincontent').getByRole('paragraph')).toHaveText('Items 1-12 of 48')
   })
+  test("Checking that the list is selected and has 10 positions by default", async ({ page }) => {
+    await page.locator('#ui-id-5').hover();
+    await page.locator('#ui-id-17').click();
+    await page.getByRole('link',{ name: 'View as  List' }).click();
+
+    await expect(page.locator('strong[title="List"]').first()).toHaveClass(/active/)
+    await expect(page.locator('li[class = "item product product-item"]')).toHaveCount(10)
+    await expect(page.locator('#maincontent').getByRole('paragraph')).toHaveText('Items 1-10 of 48')
+    
+  })
 })
