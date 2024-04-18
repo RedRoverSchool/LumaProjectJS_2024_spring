2352c24a5b70176748e63e94f059b9fa0f8bfb13
NataliaKengil <241283nata@gmail.com>
2024-04-18 20:17:29 +0300

    TC 14.1.4_01 Women/Bottoms/Shopping options/Price filter is displayed.

---
+++ tests/womenBottomsPriceFilter.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from "@playwright/test";
+
+test.describe('Women/Bottoms/Shopping options/Price', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto("/women/bottoms-women.html");
+    })
+
+    test.only('Checking that dropdown menu contains five options price filter', async ({ page }) => {
+        await page.locator('.filter-options-title').nth(4).click();
+
+        await expect(page.locator('.filter-options-item allow active')).toBeVisible;
+    })
+})
+    
