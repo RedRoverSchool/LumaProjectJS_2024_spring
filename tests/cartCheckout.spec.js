ddd2345b46b07c56518595caef4f2e10121b41c1
Natalia Starkova <n.m.starkova@gmail.com>
2024-04-20 10:55:26 +0300

    TC 09.2_07 <Cart/Checkout> User able to type in Last Name

--- tests/cartCheckout.spec.js
+++ tests/cartCheckout.spec.js
@@ -5,8 +5,6 @@ test.describe('US Cart/Checkout', () => {
     test('TC 09_2_06 <Cart/Checkout> User able to type in their First Name', async ({ page }) => {
         await page.goto('https://magento.softwaretestingboard.com/');
 
-        //await page.getByLabel('Consent', { exact: true }).click();
-
         await page.getByRole('option', { name: 'XS' }).first().click();
         await page.getByRole('option', { name: 'Blue' }).first().click();
         await page.getByRole('button', { name: 'Add to Cart' }).first().click();
@@ -20,16 +18,16 @@ test.describe('US Cart/Checkout', () => {
         await expect(page.locator('#shipping-new-address-form input[name="firstname"]')).toHaveValue('Nata');
     })
 
-    test('TC 09.2_07 <Cart/Checkout> User able to type in Last Name', async ({page}) => {
+    test('TC 09.2_07 <Cart/Checkout> User able to type in Last Name', async ({ page }) => {
         await page.goto('https://magento.softwaretestingboard.com/');
-    
+
         await page.getByRole('option', { name: 'XS' }).first().click();
         await page.getByRole('option', { name: 'Blue' }).first().click();
-        await page.getByRole('button', {name: 'Add to Cart'}).first().click();
-    
+        await page.getByRole('button', { name: 'Add to Cart' }).first().click();
+
         await page.locator(".showcart .counter-number").click();
         await page.getByRole('button', { name: 'Proceed to Checkout' }).click();
-    
+
         await page.locator('#shipping-new-address-form div').filter({ hasText: 'Last Name' }).click();
         await page.getByLabel('Last Name').fill('Smith');
 
