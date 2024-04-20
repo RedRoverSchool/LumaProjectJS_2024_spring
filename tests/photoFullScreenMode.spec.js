3535a1f27ab94f757e4330e70731b4e42022c5db
Yuliya Kuzz <jkuzmina84@gmail.com>
2024-04-20 09:49:11 +0400

    without timeout

--- tests/photoFullScreenMode.spec.js
+++ tests/photoFullScreenMode.spec.js
@@ -3,7 +3,6 @@ import {test, expect} from "@playwright/test"
 
 test.describe('photoFullSCreenMode', () => {
     test('photo in full screen mode', async ({page}) => {
-        test.setTimeout(50000);
         await page.goto('https://magento.softwaretestingboard.com/fusion-backpack.html'); 
 
         await page.locator('.fotorama__stage .fotorama__active .fotorama__img').click();
