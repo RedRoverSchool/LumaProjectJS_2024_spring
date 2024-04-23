f3358fb5d9f1582b803a62c58c92c56c4442b0a8
MlimaU <129126676+mlimau@users.noreply.github.com>
2024-04-22 21:47:12 -0400

    Pull request fixing acc to state

--- tests/mainMenuNavigation.spec.js
+++ tests/mainMenuNavigation.spec.js
@@ -5,16 +5,31 @@ test.describe('menu navigation', () => {
     await page.goto('/')
   })
 
-     test('verify User can see 6 menu options on the main page with particular text', async ({
-    page
-  }) => {
+  test('should be main menu and options', async ({ page }) => {
+    const navigation = page.locator('#ui-id-2')
+    const menuOptions = [
+      "What's New",
+      'Women',
+      'Men',
+      'Gear',
+      'Training',
+      'Sale'
+    ]
+    await expect(navigation).toBeVisible()
+    await expect(navigation).toHaveCSS(
+      'background',
+      'rgba(0, 0, 0, 0) none repeat scroll 0% 0% / auto padding-box border-box'
+    )
+    await expect(navigation).toBeVisible(menuOptions)
+  })
+
+  test('verify User can see 6 menu options on the main page with particular text', async ({page}) => {
     let array = ["What's New", 'Women', 'Men', 'Gear', 'Training', 'Sale']
 
     const menuOptionsLocator = page.locator('.level-top.ui-corner-all')
     const menuOptionsLocatorTexts = await menuOptionsLocator.allInnerTexts()
-  
-    expect(menuOptionsLocatorTexts).toEqual(array);
-  
-   })
 
+    expect(menuOptionsLocatorTexts).toEqual(array)
+  })
+  
 })
