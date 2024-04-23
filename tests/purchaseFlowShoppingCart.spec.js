ed80084bb76f0bb09ac453a78d1b019d0a5af6c2
Katerina Poliit <alexispoliyit@gmail.com>
2024-04-23 19:03:13 +0300

    TC 09.1.1_05 Checking the operation of the button and going to the checkout page

--- tests/purchaseFlowShoppingCart.spec.js
+++ tests/purchaseFlowShoppingCart.spec.js
@@ -90,4 +90,45 @@ test.describe('page checkout/cart', () => {
 
 })
 
+test(' buttons [Proceed to Checkout] should start and go to the checkout page', async ({ page }) => {
+
+  await page.goto('https://magento.softwaretestingboard.com/women/tops-women/jackets-women.html');
+  await page.getByText('Olivia 1/4 Zip Light Jacket').click();
+  await page.getByRole('button', { name: 'Add to Cart' }).click();
+  await page.locator('#option-label-size-143-item-168').click();
+  await page.locator('#option-label-color-93-item-50').click();
+
+  await page.getByRole('button', { name: 'My Cart' }).isVisible();
+  await page.click('button[title="Add to Cart"]');
+  await page.waitForTimeout(3000);
+
+  await page.click('a.action.showcart');
+
+  await page.getByRole('link', { name: 'View and Edit Cart' }).click();
+  await page.getByRole('combobox').click();
+
+  await page.getByRole('heading', {name: 'Estimate Shipping and Tax '}).click();
+  await page.waitForTimeout(3000);
+  await expect(page.getByRole('heading', {name: 'Estimate Shipping and Tax '})).toBeTruthy();
+
+  await page.getByRole('tab', { name: 'Estimate Shipping and Tax' }).click();
+
+  const countrySelector = 'Country';
+  await page.locator(`label:has-text("${countrySelector}") + select`, 'United States');
+  await expect(page.locator(`label:has-text("${countrySelector}") + select`, 'United States')).toBeTruthy();
+  // await page.waitForTimeout(3000);
+  // await page.getByRole('tab', { name: 'Estimate Shipping and Tax' }).click();
+
+  await page.waitForTimeout(3000);
+  await page.locator('select[name="region_id"]').click()
+   await page.waitForTimeout(3000);
+   await page.locator('select[name="region_id"]').selectOption('Alabama');
+
+   await page.waitForTimeout(3000);
+  await page.getByRole('button', { name: 'Proceed to Checkout' }).click();
+  await page.waitForTimeout(5000);
+  await expect(page).toHaveURL('https://magento.softwaretestingboard.com/checkout/#shipping');
+  
+})
+
 })
