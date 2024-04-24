69e21d481c2131f8cf495eef7e0d141a8669ed5f
Kseniya <ksenia.parfenovic@gmail.com>
2024-04-24 09:22:48 +0200

    fix prettier

--- tests/womenBottoms.spec.js
+++ tests/womenBottoms.spec.js
@@ -1,48 +1,54 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("Women bottoms page", () => {
-  const BASE_URL = "https://magento.softwaretestingboard.com";
+    const BASE_URL = "https://magento.softwaretestingboard.com";
 
-  test.beforeEach(async ({ page }) => {
-    await page.goto("/");
+    test.beforeEach(async ({ page }) => {
+        await page.goto("/");
 
-    if (
-      await page
-        .getByRole("dialog", {
-          name: "This site asks for consent to use your data",
-        })
-        .isVisible()
-    ) {
-      await page.getByLabel("Consent", { exact: true }).click();
-    }
-  });
+        if (
+            await page
+                .getByRole("dialog", {
+                    name: "This site asks for consent to use your data",
+                })
+                .isVisible()
+        ) {
+            await page.getByLabel("Consent", { exact: true }).click();
+        }
+    });
 
-  test("TC 05.2.1_01 Redirection to the Women/Bottoms page after clicking the “Bottoms” droplist category.", async ({
-    page,
-  }) => {
-    await page.getByText("Women").hover();
-    await page.locator("#ui-id-10").click();
+    test("TC 05.2.1_01 Redirection to the Women/Bottoms page after clicking the “Bottoms” droplist category.", async ({
+        page,
+    }) => {
+        await page.getByText("Women").hover();
+        await page.locator("#ui-id-10").click();
 
-    await expect(page).toHaveURL(BASE_URL + "/women/bottoms-women.html");
-    await expect(page).toHaveTitle("Bottoms - Women");
-  });
+        await expect(page).toHaveURL(BASE_URL + "/women/bottoms-women.html");
+        await expect(page).toHaveTitle("Bottoms - Women");
+    });
 
-  test('TC 05.2.1_02 Breadscrumbs "Home > Women > Bottoms" is displayed on the page', async ({
-    page,
-  }) => {
-    await page.getByText("Women").hover();
-    await page.locator("#ui-id-10").click();
+    test('TC 05.2.1_02 Breadscrumbs "Home > Women > Bottoms" is displayed on the page', async ({
+        page,
+    }) => {
+        await page.getByText("Women").hover();
+        await page.locator("#ui-id-10").click();
 
-    await expect(page.locator(".breadcrumbs")).toHaveText("Home Women Bottoms");
-  });
+        await expect(page.locator(".breadcrumbs")).toHaveText(
+            "Home Women Bottoms"
+        );
+    });
 
-  test("Product display mode change in the catalog", async ({ page }) => {
-    await page.goto("/" + "women/bottoms-women.html");
-    await page.getByTitle("List").first().click();
-    expect(await page.locator("div[class*=products-list]")).toBeVisible();
-    expect(await page.locator("div[class*=products-grid] ")).not.toBeVisible();
-    await page.getByTitle("Grid").first().click();
-    expect(await page.locator("div[class*=products-grid]")).toBeVisible();
-    expect(await page.locator("div[class*=products-list] ")).not.toBeVisible();
-  });
+    test("Product display mode change in the catalog", async ({ page }) => {
+        await page.goto("/" + "women/bottoms-women.html");
+        await page.getByTitle("List").first().click();
+        expect(await page.locator("div[class*=products-list]")).toBeVisible();
+        expect(
+            await page.locator("div[class*=products-grid] ")
+        ).not.toBeVisible();
+        await page.getByTitle("Grid").first().click();
+        expect(await page.locator("div[class*=products-grid]")).toBeVisible();
+        expect(
+            await page.locator("div[class*=products-list] ")
+        ).not.toBeVisible();
+    });
 });
