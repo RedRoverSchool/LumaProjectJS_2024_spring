bd33f1b5552f5f0b7ed7af3eaeb0c51b54847a65
Eduard-QA <eduard.mickel@gmail.com>
2024-04-25 14:48:40 +0300

    TC 11.2.1_01 Сheck if the link "My Orders" is clickable

--- tests/myOrders.spec.js
+++ tests/myOrders.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("customerAccount", () => {
+test.describe("myOrders", () => {
 
     function generateRandomEmail() {
         const mailbox = Math.random().toString(36).substring(2, 10);
@@ -20,28 +20,19 @@ test.describe("customerAccount", () => {
         await page.getByRole("textbox", { name: "First Name*" }).fill(firstname);
         await page.getByRole("textbox", { name: "Last Name*" }).fill(lastname);
         await page.getByRole("textbox", { name: "Email*" }).fill(email);
-        await page
-            .getByRole("textbox", { name: "Password*", exact: true })
-            .fill(password);
-        await page
-            .getByRole("textbox", { name: "Confirm Password*" })
-            .fill(password);
+        await page.getByRole("textbox", { name: "Password*", exact: true }).fill(password);
+        await page.getByRole("textbox", { name: "Confirm Password*" }).fill(password);
         await page.getByRole("button", { name: "Create an Account" }).click();
-        await page.getByRole("link", { name: "store logo" }).click();
+
     });
 
-    test("Customer name is displaied on the home page", async ({ page }) => {
-        const greetingMSG = page
-            .getByRole("banner")
-            .getByText("Welcome, Angelina-Maria O'Neel!");
+    test('Check "My Orders" link is clickable', async ({ page }) => {
+        await page.getByRole('link', { name: 'My Orders' }).click();
+        await page.waitForURL(BASE_URL + '/sales/order/history/');
+        await expect(page.getByRole('heading', { name: 'My Orders' })).toBeVisible();
+        await expect(page).toHaveURL('https://magento.softwaretestingboard.com/sales/order/history/');
 
-        await expect(greetingMSG).toBeVisible();
-        await expect(greetingMSG).toHaveText("Welcome, Angelina-Maria O'Neel!");
     });
-    
+
 });
 
-// Test
-// Test
-// micked67@gmail.com
-// Password1
