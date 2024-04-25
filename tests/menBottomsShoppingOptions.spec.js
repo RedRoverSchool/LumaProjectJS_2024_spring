dd96137e6f15226c461f1e40e5966e00d0feec23
Vera Harbachova <vera.harbachova@outlook.com>
2024-04-25 21:19:11 +0200

    TC 16.1.1_03 Men/Bottoms/Shopping options/Category Verify the user can select a subcategory from the dropdown

--- tests/menBottomsShoppingOptions.spec.js
+++ tests/menBottomsShoppingOptions.spec.js
@@ -25,4 +25,33 @@ test.describe('MenBottomsShoppingOptions', () => {
 
         expect(sidebarLeft).toBe('left');
     })
+
+    test('verify the user can select a subcategory from the dropdown', async ({ page }) => {
+        const expectedSubCategorySelector = [
+            '.filter-options-item.allow.active > div.filter-options-content > ol > li:nth-child(1) > a', 
+            '.filter-options-item.allow.active > div.filter-options-content > ol > li:nth-child(2) > a'
+        ];
+        
+        const categoryTitles = [
+            'Pants', 
+            'Shorts'
+        ];
+
+        const categoryLinks = [
+            'https://magento.softwaretestingboard.com/men/bottoms-men.html?cat=18',
+            'https://magento.softwaretestingboard.com/men/bottoms-men.html?cat=19'
+        ];
+
+            for (let i = 0; i < expectedSubCategorySelector.length; i++) {
+                await page.locator('#ui-id-5').hover();
+                await page.locator('#ui-id-18').click();
+                await expect(page).toHaveURL('https://magento.softwaretestingboard.com/men/bottoms-men.html');
+                await page.locator('.filter-options-title').getByText('Category').hover();
+                await page.locator('.filter-options-title').getByText('Category').click();
+                
+                await page.locator(expectedSubCategorySelector[i]).click();
+                await expect(page.locator(`.filter-value:has-text('${categoryTitles[i]}')`)).toContainText(categoryTitles[i]);
+                await expect(page).toHaveURL(categoryLinks[i]);
+            }
+    })
 })
