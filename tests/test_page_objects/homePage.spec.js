6e1d72a3c37cd712142965a24fd001f3a3394795
MariaMoroz <morozova.mary@gmail.com>
2024-04-25 22:10:49 -0400

    removed .only

--- tests/test_page_objects/homePage.spec.js
+++ tests/test_page_objects/homePage.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 import HomePage from "../../page_objects/homePage.js";
 import { BASE_URL, WHATS_NEW_PAGE_END_POINT, WHATS_NEW_PAGE_HEADER } from "../../helpers/testData.js";
 
-test.describe.only('homePage.spec', () => {
+test.describe('homePage.spec', () => {
     test('verify user can navigate to home page clicking on logo from "What\'s New" page', async ({ page }) => {
         const homePage = new HomePage(page);
 
