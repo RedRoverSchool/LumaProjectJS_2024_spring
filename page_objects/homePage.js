82aae6fdd068f5ea50b6f94ef2c366e4f1beff65
AlexUA2022 <avsaponov@gmail.com>
2024-04-27 13:47:22 +0300

    Add comma

--- page_objects/homePage.js
+++ page_objects/homePage.js
@@ -19,7 +19,7 @@ class HomePage {
     getAutocompleteSearchItems: () => this.page.locator("#search_autocomplete>ul>li>span:first-child"),
     getSearchButton: () => this.page.locator('button[title="Search"]'),
     getRadiantTee: () => this.page.getByTitle('Radiant Tee'),
-	 getTrainingLink: () => this.page.getByRole('menuitem', { name: 'Training' })
+	 getTrainingLink: () => this.page.getByRole('menuitem', { name: 'Training' }),
     getCreateAccountLink: () => this.page.getByRole('link', {name: 'Create an Account'})
   };
 
