d6db43d39b3560041060021b89fc0135dce74f16
trukhachev495 <sergey.trainings@gmail.com>
2024-04-27 18:19:16 +0400

    RF |menTops.spec.js | Check the name of 14 shopping styles in the Men's/Tops section.1

--- tests/test_page_objects/menTops.spec.js
+++ tests/test_page_objects/menTops.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 import HomePage from "../../page_objects/homePage.js";
-import {LIST_STYLE_MEN_TOPS} from "../../helpers/testData.js"
+import {LIST_STYLE_MEN_TOPS,BASE_URL,MEN_TOPS_PAGE_END_POINT} from "../../helpers/testData.js"
 import MenTopsPage from "../../page_objects/menTopsPage.js";
 test.describe('menTops', () => {
     test.beforeEach(async ({ page }) => {
@@ -18,5 +18,6 @@ test("Check the name of 14 shopping styles in the Men's/Tops section.", async ({
     for (let index = 0; index < LIST_STYLE_MEN_TOPS.length; index++) {
         await expect(menTopsPage.locators.getMenTopsListStyle().nth(index)).toContainText(LIST_STYLE_MEN_TOPS[index])
         }
+    await expect(page).toHaveURL(BASE_URL + MEN_TOPS_PAGE_END_POINT)
    });
 })
