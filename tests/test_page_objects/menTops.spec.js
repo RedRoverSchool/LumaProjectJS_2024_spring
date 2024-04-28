27072f7deefb6cb41e1a433f2b230803a071fb7b
darin4 <zvetochek23@mail.ru>
2024-04-28 17:20:16 +0300

    progress

--- tests/test_page_objects/menTops.spec.js
+++ tests/test_page_objects/menTops.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 import HomePage from "../../page_objects/homePage.js";
-import {LIST_STYLE_MEN_TOPS,BASE_URL,MEN_TOPS_PAGE_END_POINT} from "../../helpers/testData.js"
+import {LIST_STYLE_MEN_TOPS,BASE_URL,MEN_TOPS_PAGE_END_POINT,LIST_CATEGORY_MEN_TOPS} from "../../helpers/testData.js"
 import MenTopsPage from "../../page_objects/menTopsPage.js";
 test.describe('menTops', () => {
     test.beforeEach(async ({ page }) => {
@@ -20,4 +20,21 @@ test("Check the name of 14 shopping styles in the Men's/Tops section.", async ({
         }
     await expect(page).toHaveURL(BASE_URL + MEN_TOPS_PAGE_END_POINT)
    });
+   test('check quantity of items is displayed', async ({ page }) => {
+    const homePage = new HomePage(page)
+    const menTopsPage = new MenTopsPage(page)
+
+    await homePage.hoverMenLink()
+    await homePage.clickMenTopsLink()
+    await homePage.clickMenTopsCategory()
+
+   // await page.getByRole('tab', { name: 'Category' }).click();
+    //const categOptions = page.locator('#narrow-by-list').getByRole('tabpanel');
+    //await page.click(categOptions);
+    //const catOptArray = ['Jackets 11 item', 'Hoodies & Sweatshirts 13 item', 'Tees 12 item', 'Tanks 12 item'];
+   // catOptArray.forEach(item => {
+    //    expect(/\d+/.test(item)).toBe(true)
+   // })
+    //await expect(categOptions).toBeVisible();
+});
 })
