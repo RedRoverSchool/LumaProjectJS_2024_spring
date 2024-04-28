afe6f5b6de517deb9d5d743fe73ffd6f63e06ee6
darin4 <zvetochek23@mail.ru>
2024-04-28 18:21:14 +0300

    TC_POM 15.1.1_02 Men/Tops/Shopping options/Category

--- page_objects/menTopsPage.js
+++ page_objects/menTopsPage.js
@@ -7,15 +7,15 @@ class MenTopsPage{
     getMenTopsListStyle: () => this.page.locator('a[href*= "men/tops-men.html?style_general"]'),
     getMenTopsCategory: () => this.page.getByRole('tab', { name: 'Category' }),
     getMenTopsListCategory: () => this.page.locator('#narrow-by-list').getByRole('tabpanel'),
-   }
+   };
    async clickMenTopsStyle(){
     await this.locators.getMenTopsStyle().click()
 
     return this
-   }
+   };
    async clickMenTopsCategory(){
       await this.locators.getMenTopsCategory().click();
-      return this.page;
-   }
-}
+      return new MenTopsPage(this.page);
+   };
+ }
 export default MenTopsPage
--- tests/test_page_objects/menTops.spec.js
+++ tests/test_page_objects/menTops.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 import HomePage from "../../page_objects/homePage.js";
-import {LIST_STYLE_MEN_TOPS,BASE_URL,MEN_TOPS_PAGE_END_POINT,LIST_CATEGORY_MEN_TOPS} from "../../helpers/testData.js"
+import { LIST_STYLE_MEN_TOPS, BASE_URL, MEN_TOPS_PAGE_END_POINT, LIST_CATEGORY_MEN_TOPS } from "../../helpers/testData.js"
 import MenTopsPage from "../../page_objects/menTopsPage.js";
 test.describe('menTops', () => {
     test.beforeEach(async ({ page }) => {
@@ -8,33 +8,29 @@ test.describe('menTops', () => {
 
         await homePage.open();
     })
-test("Check the name of 14 shopping styles in the Men's/Tops section.", async ({ page }) => {
-    const homePage = new HomePage(page)
-    const menTopsPage = new MenTopsPage(page)
+    test("Check the name of 14 shopping styles in the Men's/Tops section.", async ({ page }) => {
+        const homePage = new HomePage(page)
+        const menTopsPage = new MenTopsPage(page)
 
-    await homePage.hoverMenLink()
-    await homePage.clickMenTopsLink()
-    await menTopsPage.clickMenTopsStyle()
-    for (let index = 0; index < LIST_STYLE_MEN_TOPS.length; index++) {
-        await expect(menTopsPage.locators.getMenTopsListStyle().nth(index)).toContainText(LIST_STYLE_MEN_TOPS[index])
+        await homePage.hoverMenLink()
+        await homePage.clickMenTopsLink()
+        await menTopsPage.clickMenTopsStyle()
+        for (let index = 0; index < LIST_STYLE_MEN_TOPS.length; index++) {
+            await expect(menTopsPage.locators.getMenTopsListStyle().nth(index)).toContainText(LIST_STYLE_MEN_TOPS[index])
         }
-    await expect(page).toHaveURL(BASE_URL + MEN_TOPS_PAGE_END_POINT)
-   });
-   test('check quantity of items is displayed', async ({ page }) => {
-    const homePage = new HomePage(page)
-    const menTopsPage = new MenTopsPage(page)
+        await expect(page).toHaveURL(BASE_URL + MEN_TOPS_PAGE_END_POINT)
+    });
+    test('check quantity of items is displayed', async ({ page }) => {
+        const homePage = new HomePage(page);
+        const menTopsPage = new MenTopsPage(page);
 
-    await homePage.hoverMenLink()
-    await homePage.clickMenTopsLink()
-    await homePage.clickMenTopsCategory()
-
-   // await page.getByRole('tab', { name: 'Category' }).click();
-    //const categOptions = page.locator('#narrow-by-list').getByRole('tabpanel');
-    //await page.click(categOptions);
-    //const catOptArray = ['Jackets 11 item', 'Hoodies & Sweatshirts 13 item', 'Tees 12 item', 'Tanks 12 item'];
-   // catOptArray.forEach(item => {
-    //    expect(/\d+/.test(item)).toBe(true)
-   // })
-    //await expect(categOptions).toBeVisible();
-});
+        await homePage.hoverMenLink();
+        await homePage.clickMenTopsLink();
+        await page.waitForTimeout(6000);
+        await menTopsPage.clickMenTopsCategory();
+        LIST_CATEGORY_MEN_TOPS.forEach(item => {
+            expect(/\d+/.test(item)).toBe(true)
+        })
+        await expect(menTopsPage.locators.getMenTopsListCategory()).toBeVisible();
+    });
 })
