f4fe173c7962b60fa7b6bd7ce17d0a21f941f85e
NataliaKengil <241283nata@gmail.com>
2024-04-29 12:56:18 +0300

    RF | womenBottomsPriceFilter.spec | TC 14.1.4_01 Women/Bottoms/Shopping options/Price filter is displayed.

--- page_objects/bottomsWomenPage.js
+++ page_objects/bottomsWomenPage.js
@@ -6,7 +6,10 @@ class BottomsWomenPage {
         getWomenBottomsPageHeader: () => this.page.getByRole('heading', { name: 'Bottoms' }),
         getWomenBottomsOptionStyle: () => this.page.getByRole('tab', { name: 'Style' }),
         getAriaSelectedWomenBottoms: () => this.page.locator('[aria-selected]'),
-        getCategoryInStyle: () => this.page.locator('[aria-hidden="false"] .items>.item')
+        getCategoryInStyle: () => this.page.locator('[aria-hidden="false"] .items>.item'),
+        getOptionPrice: () => this.page.locator('.filter-options-title').nth(3),
+        getOptionPriceFilter: () => this.page.locator('.filter-options-content').nth(3)
+
     }
 
     async clickWomenBottomsOptionStyle() {
@@ -19,6 +22,12 @@ class BottomsWomenPage {
         const areEqual = JSON.stringify(extractedItems) === JSON.stringify(expectedItems);
         
         return { extractedItems, areEqual };
+    }
+
+    async clickOptionPrice(){
+        await this.locators.getOptionPrice().click();
+
+        return this;
     }
 }
 export default BottomsWomenPage;
--- tests/test_page_objects/bottomsWomenPage.spec.js
+++ tests/test_page_objects/bottomsWomenPage.spec.js
@@ -1,44 +1,80 @@
 import { test, expect } from "@playwright/test";
 import HomePage from "../../page_objects/homePage.js";
 import BottomsWomenPage from "../../page_objects/bottomsWomenPage.js";
-import { BASE_URL, BOTTOMS_WOMEN_PAGE_END_POINT, WOMEN_BOTTOMS_HEADER, EXPECTED_ITEM_STYLE_WOMEN_BOTTOMS} from "../../helpers/testData.js";
+import {
+  BASE_URL,
+  BOTTOMS_WOMEN_PAGE_END_POINT,
+  WOMEN_BOTTOMS_HEADER,
+  EXPECTED_ITEM_STYLE_WOMEN_BOTTOMS,
+} from "../../helpers/testData.js";
 
-test.describe('bottomsWomenPage.spec', () => {
-    test.beforeEach(async ({ page }) => {
-        const homePage = new HomePage(page);
+test.describe("bottomsWomenPage.spec", () => {
+  test.beforeEach(async ({ page }) => {
+    const homePage = new HomePage(page);
 
-        await homePage.open();
-    })
+    await homePage.open();
+  });
 
-    test('Verify that the user can navigate from the home page to the "Women - Bottoms" page', async ({ page }) => {
-        const homePage = new HomePage(page);
-        const bottomsWomenPage = new BottomsWomenPage(page);
+  test('Verify that the user can navigate from the home page to the "Women - Bottoms" page', async ({
+    page,
+  }) => {
+    const homePage = new HomePage(page);
+    const bottomsWomenPage = new BottomsWomenPage(page);
 
-        await homePage.hoverWomenMenuitem();
-        await homePage.clickBottomsWomenLink();
-        
-        await expect(page).toHaveURL(BASE_URL + BOTTOMS_WOMEN_PAGE_END_POINT);
-        await expect(bottomsWomenPage.locators.getWomenBottomsPageHeader()).toHaveText(WOMEN_BOTTOMS_HEADER);
-      });
+    await homePage.hoverWomenMenuitem();
+    await homePage.clickBottomsWomenLink();
 
-      test('Verify the availability of a list of 9 category in the "Style" option drop-down list', async ({ page }) => {
-        const homePage = new HomePage(page);
-        const bottomsWomenPage = new BottomsWomenPage(page);
+    await expect(page).toHaveURL(BASE_URL + BOTTOMS_WOMEN_PAGE_END_POINT);
+    await expect(
+      bottomsWomenPage.locators.getWomenBottomsPageHeader()
+    ).toHaveText(WOMEN_BOTTOMS_HEADER);
+  });
 
-        await homePage.hoverWomenMenuitem();
-        await homePage.clickBottomsWomenLink();
-        
-        await expect(page).toHaveURL(BASE_URL + BOTTOMS_WOMEN_PAGE_END_POINT);
-
-        await bottomsWomenPage.clickWomenBottomsOptionStyle();
+  test.beforeEach(async ({ page }) => {
     
-        expect(await bottomsWomenPage.locators.getAriaSelectedWomenBottoms()).toBeTruthy();
-    
-        const expectedItems = EXPECTED_ITEM_STYLE_WOMEN_BOTTOMS;
-        const receivedResult = await bottomsWomenPage.locators.getCategoryInStyle().allInnerTexts();
-    
-        const result = await bottomsWomenPage.extractAndCompareItems(receivedResult, expectedItems);
-    
-        expect(result.extractedItems).toEqual(expectedItems);
-    });
+    const homePage = new HomePage(page);
+    const bottomsWomenPage = new BottomsWomenPage(page);
+
+    await homePage.hoverWomenMenuitem();
+    await homePage.clickBottomsWomenLink();
+  });
+
+
+  test('Verify the availability of a list of 9 category in the "Style" option drop-down list', async ({
+    page,
+  }) => {
+    const homePage = new HomePage(page);
+    const bottomsWomenPage = new BottomsWomenPage(page);
+
+    await homePage.hoverWomenMenuitem();
+    await homePage.clickBottomsWomenLink();
+
+    await expect(page).toHaveURL(BASE_URL + BOTTOMS_WOMEN_PAGE_END_POINT);
+
+    await bottomsWomenPage.clickWomenBottomsOptionStyle();
+
+    expect(
+      await bottomsWomenPage.locators.getAriaSelectedWomenBottoms()
+    ).toBeTruthy();
+
+    const expectedItems = EXPECTED_ITEM_STYLE_WOMEN_BOTTOMS;
+    const receivedResult = await bottomsWomenPage.locators
+      .getCategoryInStyle()
+      .allInnerTexts();
+
+    const result = await bottomsWomenPage.extractAndCompareItems(
+      receivedResult,
+      expectedItems
+    );
+
+    expect(result.extractedItems).toEqual(expectedItems);
+  });
+
+  test('Women/Bottoms/Shopping options/Price filter is displayed', async ({ page }) => {
+
+    const bottomsWomenPage = new BottomsWomenPage(page);
+    await bottomsWomenPage.clickOptionPrice();
+
+    await expect(bottomsWomenPage.locators.getOptionPriceFilter()).toBeVisible();
 })
+});
