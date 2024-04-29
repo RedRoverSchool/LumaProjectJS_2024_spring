3e9b4254f27973ee4fc5923bb7d3a11d9a28afc0
verafes <verafes@gmail.com>
2024-04-28 19:55:26 -0700

    TC 05.1.9_01 Women/Tops/AddToWishList | Verify message in WishList section for Empty Wish List

--- helpers/testData.js
+++ helpers/testData.js
@@ -89,8 +89,7 @@ export const MY_ORDERS_HEADER = 'My Orders';
 export const WOMEN_JACKETS_NAME = 'Inez Full Zip Jacket';
 export const INEZ_FULL_ZIP_JACKET1_END_POINT = '/inez-full-zip-jacket.html';
 export const SHOPPING_CART_END_POINT = '/checkout/cart/';
-
-
+export const MY_WISH_LIST_EMPTY_MESSAGE ='You have no items in your wish list.'
 
 
 //login  credential
