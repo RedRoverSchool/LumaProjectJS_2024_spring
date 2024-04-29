cb70c8f9edd25d9f3951288dc213775f3bcf8ad0
MlimaU <129126676+mlimau@users.noreply.github.com>
2024-04-29 17:37:57 -0400

    RF-POM | mainMenu.spec | TC 04.7.1_2 verify 6 menu options on the main page have particular text and clickable

--- page_objects/mainMenu.js
+++ page_objects/mainMenu.js
@@ -17,19 +17,5 @@ class MainMenuPage {
 
     return mainMenuLinksText
   }
-  async clickMainMenuLinksAndCheckRedirect (NAVBAR_MENU, NAVBAR_URLs) {
-    const links = await this.locators.getMainMenuLinks() // Получаем ссылки из локатора
-   
-
-      for (let i = 0; i < links.length; i++) {
-        const link = links[i]
-      const text = await link.innerText()
-      expect(text).toEqual(NAVBAR_MENU[i]) // Проверяем текст ссылки
-      await link.click() // Выполняем клик по ссылке
-      await expect(page).toHaveURL(NAVBAR_URLs[i]) // Проверяем URL страницы после перехода
-      await page.goBack() // Возвращаемся на предыдущую страницу
-      // Добавьте здесь дополнительные проверки или действия, если необходимо
-    }
-  }
 }
 export default MainMenuPage
