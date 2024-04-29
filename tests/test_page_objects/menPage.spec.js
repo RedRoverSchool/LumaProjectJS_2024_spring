44a49c75b0d96e5b3e5c116b0c1eee1f8a951504
Evgeny Umetski <evgeny.umetski@amaryllispay.com>
2024-04-29 21:09:08 +0300

    RF_POM | menPage.spec | TC 04.3.4_01 <Men page> Page contains Shop by category block which is located on the left side of the page

--- tests/test_page_objects/menPage.spec.js
+++ tests/test_page_objects/menPage.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 import HomePage from "../../page_objects/homePage.js";
-import { BASE_URL, MEN_PAGE_BOTTOMS_SUB_CATEGORY_LINK_COLOR, MEN_PAGE_TOPS_SUB_CATEGORY_LINK_COLOR, } from "../../helpers/testData.js";
+import { BASE_URL, MEN_PAGE_BOTTOMS_SUB_CATEGORY_LINK_COLOR, MEN_PAGE_SHOP_BY_CATEGORY_BLOCK_ALIGHMENT, MEN_PAGE_TOPS_SUB_CATEGORY_LINK_COLOR, } from "../../helpers/testData.js";
 import {
     MEN_PAGE_END_POINT,
     MEN_PAGE_HEADER,
@@ -13,6 +13,9 @@ test.describe('menPage.spec', () => {
     test.beforeEach(async ({ page }) => {
         const homePage = new HomePage(page);
         await homePage.open();
+        if (await homePage.getByRole('dialog', { name: 'This site asks for consent to use your data' }).isVisible()) {
+            await homePage.getByRole('button', { name: 'Consent' }).click();
+        };
     });
     test('Menu/Men available to click, see clothes only for men', async ({ page }) => {
         const homePage = new HomePage(page);
@@ -25,6 +28,16 @@ test.describe('menPage.spec', () => {
         await expect(menPage.locators.getCompareProducts()).toBeVisible(COMPARE_PRODUCTS_TEXT);
         await expect(menPage.locators.getMyWishList()).toBeVisible(MY_WISH_LIST_TEXT);
     })
+
+    test('Men page contains Shop by category block which is located on the left side of the page', async ({ page }) => {
+        const homePage = new HomePage(page);
+        const menPage = new MenPage(page);
+
+        await homePage.clickMenLink();
+        
+        await expect(menPage.locators.shopByCategoryBlock()).toBeVisible();
+        await expect(menPage.locators.shopByCategoryBlock()).toHaveCSS('float', MEN_PAGE_SHOP_BY_CATEGORY_BLOCK_ALIGHMENT);
+    });
 
     test('Category block contains sub-categories: Tops and Bottoms which are links in blue text', async ({ page }) => {
         const homePage = new HomePage(page);
