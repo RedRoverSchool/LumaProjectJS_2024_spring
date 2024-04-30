4bc88be89f22108bedb52825ef60edcaff6d556c
Svetlana Kanel <svetlana.kanel@gmail.com>
2024-04-30 13:02:42 -0400

    resolve conflicts in gearBagsPage

--- page_objects/gearBagsPage.js
+++ page_objects/gearBagsPage.js
@@ -1,14 +1,36 @@
-<<<<<<< HEAD
+import TrainingPage from "./trainingPage";
+
 class GearBagsPage {
     constructor(page) {
         this.page = page;
-    };
+    }
 
     locators = {
+        getPushItMessengerItem: () => this.page.getByRole('link', { name: 'Push It Messenger Bag' }).first(),
+        getPushItMessengerItemAddtoCampare: () => this.page.locator('li').filter({ hasText: 'Push It Messenger Bag Rating' }).getByLabel('Add to Compare'),
+        getTrainingLink: () => this.page.getByRole('menuitem', { name: 'Training' }),
         getMaterialOption: () => this.page.getByRole("tab", { name: "Material" }),
         getMateialItemList: () => this.page.locator('.filter-options>:nth-child(4) li')
+    };
+
+    async hoverPushItMessengerItem() {
+        await this.locators.getPushItMessengerItem().hover();
+
+        return this;
     }
-    
+
+    async clickgetPushItMessengerItemAddtoCampare() {
+        await this.locators.getPushItMessengerItemAddtoCampare().click();
+
+        return this;
+    }
+
+    async clickTrainingLink() {
+        await this.locators.getTrainingLink().click();
+
+        return new TrainingPage(this.page);
+    }
+
     async clickMaterialOption() {
         await this.locators.getMaterialOption().click();
 
@@ -18,40 +40,7 @@ class GearBagsPage {
     async getMaterialItemNameText(idx) {
         const text = (await this.locators.getMateialItemList().nth(idx).innerText()).split(' ')[0];
 
-        return text;    
-    }        
-=======
-import TrainingPage from "./trainingPage";
-
-class GearBagsPage {
-    constructor(page) {
-        this.page = page;
+        return text;
     }
-
-    locators = {
-		getPushItMessengerItem: () => this.page.getByRole('link', { name: 'Push It Messenger Bag' }).first(),
-		getPushItMessengerItemAddtoCampare: () => this.page.locator('li').filter({ hasText: 'Push It Messenger Bag Rating' }).getByLabel('Add to Compare'),
-		getTrainingLink: () => this.page.getByRole('menuitem', { name: 'Training' }),
-    };
-
-	 async hoverPushItMessengerItem() {
-		await this.locators.getPushItMessengerItem().hover();
-	 
-		return this;
-	 }
-
-	 async clickgetPushItMessengerItemAddtoCampare() {
-		await this.locators.getPushItMessengerItemAddtoCampare().click();
-	 
-		return this;
-	 }
-
-	 async clickTrainingLink() {
-		await this.locators.getTrainingLink().click();
-	 
-		return new TrainingPage(this.page);
-	 }
->>>>>>> 8e550eadc13ef6c68c0ef49afaf768dd2145f1cd
-
 }
 export default GearBagsPage;
