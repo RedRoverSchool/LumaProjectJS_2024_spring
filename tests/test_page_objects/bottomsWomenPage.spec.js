d917bf774e33a0e58c54a76950d7c36424b4bc29
teya-teya <teya.teya89@yandex.ru>
2024-05-02 10:59:35 +0300

    RF | bottomsWomenPage.spec.js | TC 14.1.2_05 Women/Bottoms/Shopping options/Style Verify the availability of a list of 9 category in the "Style" option drop-down list

--- tests/test_page_objects/bottomsWomenPage.spec.js
+++ tests/test_page_objects/bottomsWomenPage.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 import HomePage from "../../page_objects/homePage.js";
 import BottomsWomenPage from "../../page_objects/bottomsWomenPage.js";
-import { BASE_URL, BOTTOMS_WOMEN_PAGE_END_POINT, WOMEN_BOTTOMS_HEADER, EXPECTED_ITEM_STYLE_WOMEN_BOTTOMS} from "../../helpers/testData.js";
+import { BASE_URL, BOTTOMS_WOMEN_PAGE_END_POINT, EXPECTED_ITEM_STYLE_WOMEN_BOTTOMS} from "../../helpers/testData.js";
 import { WOMEN_BOTTOMS_CATEGORIES } from "../../helpers/testWomenData.js";
 
 test.describe('bottomsWomenPage.spec', () => {
@@ -10,27 +10,6 @@ test.describe('bottomsWomenPage.spec', () => {
 
         await homePage.open();
     })
-
-    test('Verify the availability of a list of 9 category in the "Style" option drop-down list', async ({ page }) => {
-        const homePage = new HomePage(page);
-        const bottomsWomenPage = new BottomsWomenPage(page);
-
-        await homePage.hoverWomenMenuitem();
-        await homePage.clickBottomsWomenLink();
-        
-        await expect(page).toHaveURL(BASE_URL + BOTTOMS_WOMEN_PAGE_END_POINT);
-
-        await bottomsWomenPage.clickWomenBottomsOptionStyle();
-    
-        expect(await bottomsWomenPage.locators.getAriaSelectedWomenBottoms()).toBeTruthy();
-    
-        const expectedItems = EXPECTED_ITEM_STYLE_WOMEN_BOTTOMS;
-        const receivedResult = await bottomsWomenPage.locators.getCategoryInStyle().allInnerTexts();
-    
-        const result = await bottomsWomenPage.extractAndCompareItems(receivedResult, expectedItems);
-    
-        expect(result.extractedItems).toEqual(expectedItems);
-    });
 
     test("User can able to select a category from the suggested list of 2 (two) options: Pants.", async ({ page }) => {
         const homePage = new HomePage(page);
@@ -56,5 +35,31 @@ test.describe('bottomsWomenPage.spec', () => {
         const actualShortsText =await bottomsWomenPage.getLocatorInnerText(bottomsWomenPage.locators.getShortsCategoryLocator());
 
         expect(actualShortsText).toEqual(WOMEN_BOTTOMS_CATEGORIES[1]);
+    });
+
+    test.beforeEach(async ({ page }) => {
+        const homePage = new HomePage(page);
+
+        await homePage.hoverWomenMenuitem();
+        await homePage.clickBottomsWomenLink();
+    })
+
+    test.describe('Shopping Optionls - Style', () => {
+        test('Verify the availability of a list of 9 category in the "Style" option drop-down list', async ({ page }) => {
+            const bottomsWomenPage = new BottomsWomenPage(page);
+        
+            await expect(page).toHaveURL(BASE_URL + BOTTOMS_WOMEN_PAGE_END_POINT);
+
+            await bottomsWomenPage.clickWomenBottomsOptionStyle();
+    
+            expect(await bottomsWomenPage.locators.getAriaSelectedWomenBottoms()).toBeTruthy();
+    
+            const expectedItems = EXPECTED_ITEM_STYLE_WOMEN_BOTTOMS;
+            const receivedResult = await bottomsWomenPage.locators.getCategoryInStyle().allInnerTexts();
+    
+            const result = await bottomsWomenPage.extractAndCompareItems(receivedResult, expectedItems);
+    
+            expect(result.extractedItems).toEqual(expectedItems);
+        });
     })
 })
