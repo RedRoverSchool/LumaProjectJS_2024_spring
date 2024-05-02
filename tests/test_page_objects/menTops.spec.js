6f9025282193d4e03aa52ad0ccd9b5d47e261e31
Andrey Bondalovsky <andreybondalovsky@gmail.com>
2024-05-02 23:12:35 +0300

    TC 15.1.4_01 Verify that Men/Tops/Shopping option's price filter drop-down has 8 options

--- tests/test_page_objects/menTops.spec.js
+++ tests/test_page_objects/menTops.spec.js
@@ -8,6 +8,7 @@ test.describe('menTops', () => {
 
         await homePage.open();
     })
+
     test("Check the name of 14 shopping styles in the Men's/Tops section.", async ({ page }) => {
         const homePage = new HomePage(page)
         const menTopsPage = new MenTopsPage(page)
@@ -20,6 +21,7 @@ test.describe('menTops', () => {
         }
         await expect(page).toHaveURL(BASE_URL + MEN_TOPS_PAGE_END_POINT)
     });
+
     test('check quantity of items is displayed', async ({ page }) => {
         const homePage = new HomePage(page);
         const menTopsPage = new MenTopsPage(page);
@@ -33,6 +35,7 @@ test.describe('menTops', () => {
         })
         await expect(menTopsPage.locators.getMenTopsListCategory()).toBeVisible();
     });
+
     test('displays the number of available products in the Insulated(5) category', async ({page}) => {
         const homePage = new HomePage(page)
         const menTopsPage = new MenTopsPage(page)
@@ -42,5 +45,19 @@ test.describe('menTops', () => {
         await menTopsPage.clickMenTopsStyle();
 
         await expect(menTopsPage.locators.getMenTopsStyleInsulated()).toBeVisible();
+    })
+
+    test('check Men/Tops price filter drop-down has 8 options', async ({page}) => {
+        const homePage = new HomePage(page);
+        const menTopsPage = new MenTopsPage(page);
+
+        const expectedDropdownList = '$10.00 - $19.99 , $20.00 - $29.99, $30.00 - $39.99, $40.00 - $49.99, $50.00 - $59.99, $60.00 - $69.99, $70.00 - $79.99, $90.00 and above';
+
+        await homePage.hoverMenLink();
+        await homePage.clickMenTopsLink();
+        await menTopsPage.clickMenTopsPrice();
+        let temp = await menTopsPage.locators.getMenTopsListPrice();
+        let actualDropdownList = temp;
+        await expect(actualDropdownList).toHaveText(expectedDropdownList);
     })
 })
