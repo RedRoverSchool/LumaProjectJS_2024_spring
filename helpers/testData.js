f552486dcb87aaf41758888f320bd28a7a802ebe
Katerina Poliit <alexispoliyit@gmail.com>
2024-05-04 17:00:55 +0300

    change

--- helpers/testData.js
+++ helpers/testData.js
@@ -21,7 +21,7 @@ export const ARGUS_ALL_WEATHER_TANK_PAGE_END_POINT = '/argus-all-weather-tank.ht
 export const HERO_HOODIE_PAGE_END_POINT = '/hero-hoodie.html';
 export const SIGN_IN_PAGE_END_POINT = "/customer/account/login/referer/*";
 export const FUSION_BACKPACK_END_POINT = '/fusion-backpack.html';
-export const PIERCE_GYM_SHORT = "https://magento.softwaretestingboard.com/wishlist/index/configure/id/158/product_id/1028/";
+export const PIERCE_GYM_SHORT = "https://magento.softwaretestingboard.com/wishlist/index/configure/id/164/product_id/1028/";
 
 export const LIST_OF_URLS_MENS_DEALS_END_POINT = [
   '/men/tops-men/hoodies-and-sweatshirts-men.html',
