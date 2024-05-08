f67a24b27fadf639af749f8a654cd7a11e025161
Anastasiia Go <loreley.tester@gmail.com>
2024-05-08 21:57:26 +0300

    RF_POM_gearBags/Verify bags display mode4

--- tests/test_page_objects/gearBags.spec.js
+++ tests/test_page_objects/gearBags.spec.js
@@ -79,70 +79,6 @@ test.describe('gearBags.spec', () => {
         expect(gearBagsPage.locators.getListMode()).toBeTruthy();
       })
 
-    
-
-test.describe('gearBags.spec', () => {
-    test.beforeEach(async({page}) => {
-        const homePage = new HomePage(page);
-
-        await homePage.open();
-        await homePage.hoverGearMenuItem();
-        await homePage.clickGearBags();
-    })
-
-     test('Redirect to "Gear Bags" page', async({page}) => {       
-        const gearBagsPage = new GearBagsPage(page);
-       
-        await expect(page).toHaveURL(BASE_URL + GEAR_BAGS_PAGE_END_POINT);
-        await expect(gearBagsPage.locators.getGearBagsPageHeader()).toHaveText(GEAR_BAGS_HEADER);
-    }) 
-    
-    MATERIAL_OPTION_NAMES.forEach((name, idx) => {
-        test(`Verify that ${name} from material options list is visible and has right name`, async ({ page }) => {
-            const gearBagsPage = new GearBagsPage(page);
-
-            await gearBagsPage.clickMaterialOption();
-
-            const materialName = await gearBagsPage.locators.getMateialItemList().nth(idx)
-            const materialNameText = await gearBagsPage.getMaterialItemNameText(idx);
-            
-            expect(materialName).toBeVisible();
-            expect(materialNameText).toEqual(MATERIAL_OPTION_NAMES[idx]);           
-        })
-    })
-    
-    test('BTN "Page" redirects to the corresponding page', async ({ page }) => {
-        const homePage = new HomePage(page)     
-
-        await homePage.hoverGearMenuItem()
-        const gearBagsPage = await homePage.clickGearBags()
-        await gearBagsPage.clickInactiveSecondPagePaginationLink()
-        
-        await expect(gearBagsPage.locators.getPaginationSecondPageAttr()).toHaveText(ACTIVE_PAGE_TEXT + '2')
-        await expect(gearBagsPage.locators.getPaginationSecondPageAttr()).toHaveClass(ACTIVE_PAGE_CLASS_PAGINATION)
-        await expect(gearBagsPage.locators.getPaginationFirstPageAttr()).not.toHaveText(ACTIVE_PAGE_TEXT)
-        await expect(page).toHaveURL(BASE_URL + GEAR_BAGES_SECOND_PAGE_END_POINT)
-    })
-    
-    test('Apply filter "Leather" and verify that each bag has selected material in the description', async ({ page }) => {
-        const gearBagsPage = new GearBagsPage(page);
-
-        await gearBagsPage.clickMaterialOption();
-        await gearBagsPage.clickMaterialLeather();
-
-        const numberOfItems = await gearBagsPage.getNumberOfProductItems();
-    
-        for (let i = 0; i < numberOfItems; i++) {
-            const bagItemPage = await gearBagsPage.clickOneProduct(i);
-            await bagItemPage.clickMoreImformationPanel();
-          
-            expect(await bagItemPage.getMaterialInformationText()).toContain(MATERIAL_OPTION_NAMES[3]);
-                   
-            await page.goBack();
-            await page.goBack();
-        }      
-      })
-
       test ('Verify bags display mode', async({ page }) => {
         const homePage = new HomePage(page);
         const gearBagsPage = new GearBagsPage(page);
@@ -157,5 +93,4 @@ test.describe('gearBags.spec', () => {
         await expect (gearBagsPage.locators.getSelectorBagsNumber()).toHaveAttribute(SELECTOR_BAGS_NUMBER_ATTRIBUT_SELECTED);
         await expect(gearBagsPage.locators.getListButtonPressed()).toHaveAttribute('class', CLASS_LIST_BUTTON_ACTIVE_MODE);
  })
-})
 })
