e2b56daf8cd5eef80f137315c4a1895911c8843f
Eduard-QA <eduard.mickel@gmail.com>
2024-05-11 23:45:06 +0300

    create createNewOrder in base.js

--- tests/test_page_objects/myOrders.spec.js
+++ tests/test_page_objects/myOrders.spec.js
@@ -1,69 +1,31 @@
-import { test, expect } from '@playwright/test';
-import HomePage from '../../page_objects/homePage.js';
+import { expect } from '@playwright/test';
+import { test } from "./base.js";
 import MyAccountPage from '../../page_objects/myAccountPage.js';
-
-import { MY_ORDERS_PAGE_END_POINT, MY_ORDERS_HEADER, BASE_URL, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, PASSWORD_CONFIRM, ADRESS_DATA, } from "../../helpers/testData.js";
-
+import CheckoutOnepageSuccessPage from '../../page_objects/checkoutOnepageSuccessPage.js';
+import { MY_ORDERS_PAGE_END_POINT, MY_ORDERS_HEADER, BASE_URL, } from "../../helpers/testData.js";
 
 test.describe('myOrders', () => {
-    test.beforeEach(async ({ page }) => {
-        const homePage = new HomePage(page);
 
-        await homePage.open();
-        const createAccountPage = await homePage.clickCreateAccountLink();
-        await createAccountPage.clickFirstNameField();
-        await createAccountPage.fillFirstNameField(FIRST_NAME);
-        await createAccountPage.clickLastNameField();
-        await createAccountPage.fillLastNameField(LAST_NAME);
-        await createAccountPage.clickEmailField();
-        await createAccountPage.fillEmailField(EMAIL);
-        await createAccountPage.clickPasswordField();
-        await createAccountPage.fillPasswordField(PASSWORD);
-        await createAccountPage.clickConfirmPasswordField();
-        await createAccountPage.fillConfirmPasswordField(PASSWORD_CONFIRM);
-        await createAccountPage.clickCreateAccountButton();
-    })
-
-    test('checkMyOrdersLink', async ({ page }) => {
+    test('checkMyOrdersLink', async ({ page, createNewAccount }) => {
         const myAccountPage = new MyAccountPage(page);
         const myOrdersPage = await myAccountPage.clickMyOrdersLink();
 
         await expect(page).toHaveURL(BASE_URL + MY_ORDERS_PAGE_END_POINT);
         await expect(myOrdersPage.locators.getTitle()).toContainText(MY_ORDERS_HEADER);
-
     })
 
-    test('OrderHistory', async ({ page }) => {
+    test('OrderHistory', async ({ page, createNewAccount, createNewOrder }) => {
         const myAccountPage = new MyAccountPage(page);
-        const womenPage = await myAccountPage.clickWomenLink();
-        const jacketsWomenPage = await womenPage.clickWomenJacketsLink();
-        const inezFullZipJacketPage = await jacketsWomenPage.clickWomenJacketsName();
-        await inezFullZipJacketPage.clickInezJacketSizeOptionLable();
-        await inezFullZipJacketPage.clickInezJacketColorOptionLable();
-        await inezFullZipJacketPage.clickInezJacketAddToCartButton();
-        await inezFullZipJacketPage.waitForShoppingCartLink();
-        const shoppingCartPage = await inezFullZipJacketPage.clickShoppingCartLink();
-        await shoppingCartPage.waitForOrderTotalText();
-        const shippingPage = await shoppingCartPage.clickProceedToCheckoutButton();
-        await shippingPage.fillStreetNameField(ADRESS_DATA.street);
-        await shippingPage.fillCityField(ADRESS_DATA.city);
-        await shippingPage.clickSelectRegionDropdown();
-        await shippingPage.fillPostCodeField(ADRESS_DATA.postal_code);
-        await shippingPage.fillPhoneNumberField(ADRESS_DATA.phone_number);
-        await shippingPage.checkByTypeRadioButton();
-        const paymentMethodPage = await shippingPage.clickNextButton();
-        await paymentMethodPage.waitPlaceOrderButton();
-        const checkoutOnepageSuccessPage = await paymentMethodPage.clickPlaceOrderButton();
-        await checkoutOnepageSuccessPage.waitTankYouText();
-
+        const checkoutOnepageSuccessPage = new CheckoutOnepageSuccessPage(page);
+        await checkoutOnepageSuccessPage.waitContinueShoppingButton();
         await checkoutOnepageSuccessPage.clickActionSwitchButton();
+        await checkoutOnepageSuccessPage.waitMyAccountLink();
         await checkoutOnepageSuccessPage.clickMyAccountLink();
         const myOrders = await myAccountPage.clickMyOrdersLink();
-        
+        await myOrders.waitgetTitle();
+
         await expect(myOrders.locators.getViewOrdersLink()).toBeVisible();
         await expect(myOrders.locators.getReorderLink()).toBeVisible();
-
     })
-
 })
 
