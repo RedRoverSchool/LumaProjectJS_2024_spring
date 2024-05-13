c7d75a38b2e5702e378ebdad519a637fbd2f7d8d
Laysan Nova <laysansh@gmail.com>
2024-05-13 08:09:47 -0600

    setupPage fixture

---
+++ tests/test_page_objects/setupPage.js
@@ -0,0 +1,25 @@
+import { test as base } from '@playwright/test';
+import HomePage from '../../page_objects/homePage';
+import GearPage from '../../page_objects/gearPage';
+
+export const test = base.extend({
+    gearPage: [
+        async ({ page }, use) => {
+            const homePage = new HomePage(page);
+            await homePage.open();
+            const gearPage =  await homePage.clickGearMenuItem();   
+
+            await use(gearPage);
+        },
+        { scope: "test" },
+    ],
+
+    homePage: [
+        async ({ page }, use) => {
+            const homePage = new HomePage(page);
+            await use(homePage);
+        },
+        { scope: "test" },
+    ],
+
+});
