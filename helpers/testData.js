bb2980ad1e1515233fdaac153291921e883b40a0
Andrei Novikau <andrei.novikau92@gmail.com>
2024-05-13 22:31:47 -0500

    removing unneeded code

--- helpers/testData.js
+++ helpers/testData.js
@@ -1,7 +1,5 @@
 import { expect } from "@playwright/test";
 
-import exp from "constants";
-
 //URL
 export const BASE_URL = 'https://magento.softwaretestingboard.com';
 export const WHATS_NEW_PAGE_END_POINT = '/what-is-new.html';
